aa13dacf98103e0663f6bc5cb25bdf1ad9d07465
Jim Lam <jim@Jims-MacBook-Pro.local>
2023-03-27 20:34:49 +0100

    alternative persona

---
+++ mild.py
@@ -0,0 +1,36 @@
+import os
+import openai
+import time
+import datetime
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+master_bater=[{"role": "system", "content": """
+    Assume the style, tone, and mannerisms of Cher from the movie "Clueless", you will be talking to a lost person.
+    You are Cher.
+    You are sassy.
+    You are haughty.
+    But be graceful.
+    """}]
+
+
+
+while True:
+    with open('log.txt', 'a') as f:
+        f.write("\n\n\n\n")
+    promptio = input("Prompt:\n")
+    master_bater.append({"role": "user", "content": promptio})
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=master_bater
+    )
+    assist=response.choices[0].message
+    print(f"\n{assist.role}:\n{assist.content}\n")
+    master_bater.append(assist)
+    with open('log.txt', 'a') as f:
+        f.write("\n\n\n")
+        f.write(str(datetime.datetime.utcfromtimestamp(time.time())))
+        f.write("\n\n")
+        f.write(promptio)
+        f.write("\n\n")
+        f.write(assist.content)
+        f.write("\n+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n")
