45f79af229ddab76df6741e8a5df79b2a30ac130
Jim Sebastian Lam <jim.sebastian.lam@gmail.com>
2023-03-27 22:29:30 +0100

    modularisation

--- main.py
+++ main.py
@@ -3,23 +3,28 @@ import openai
 import time
 import datetime
 import sys
+import json
 
+personas = json.load(open('personas.json', 'r'))
 openai.api_key = os.getenv("OPENAI_API_KEY")
-context_acc=[{"role": "system", "content": """
-    Assume the style, tone, and mannerisms of Cher from the movie "Clueless", you will be talking to your boss.
-    You have stockholm syndrome.
-    """}]
 
-
+display = list(personas.keys())
+while True:
+    choice = input(f"Choose your fighter:\n{display}\n")
+    if choice in personas:
+        context_acc = [{"role": "system", "content": personas[choice]}]
+        break
+    else:
+        print("Your choice is not available")
 
 while True:
     with open('log.txt', 'a') as f:
-        f.write("\n\n\n\n")
+        f.write(f"\n\nSession at {str(datetime.datetime.utcfromtimestamp(time.time()))}")
 
     try:
         promptio = input("Prompt:\n")
     except KeyboardInterrupt:
-        print("Gone")
+        print("\nAuf Wiedersehen!")
         sys.exit()
 
     context_acc.append({"role": "user", "content": promptio})
@@ -27,14 +32,16 @@ while True:
         model="gpt-3.5-turbo",
         messages=context_acc
     )
+
     assist=response.choices[0].message
     print(f"\n{assist.role}:\n{assist.content}\n")
     context_acc.append(assist)
+
     with open('log.txt', 'a') as f:
-        f.write("\n\n\n")
+        f.write("\n\n")
         f.write(str(datetime.datetime.utcfromtimestamp(time.time())))
         f.write("\n\n")
         f.write(promptio)
         f.write("\n\n")
         f.write(assist.content)
-        f.write("\n+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n")
+        f.write("\n\n")
