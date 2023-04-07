4db3bfc46e7ca3bc228ed30b5a319079222a8d89
Jim Sebastian Lam <jim.sebastian.lam@gmail.com>
2023-04-07 19:15:39 +0100

    refined UX

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import io
 import os
 import openai
-import time
 import datetime
 import sys
 import json
@@ -9,21 +8,77 @@ import pyttsx3
 import speech_recognition as sr
 
 def time_str():
-    return str(datetime.datetime.utcfromtimestamp(time.time()))
+    return str(datetime.datetime.utcnow())
 start_time = time_str()
 
 # Resource gathering
 log_file = "log.json"
 model_log = "models.json"
 personas = json.load(open('personas.json', 'r'))
+persona_options = list(personas.keys())
+persona_display = dict(enumerate(persona_options, 1))
+print(persona_display)
+
+
+# OpenAI init
+openai.api_key = os.getenv("OPENAI_API_KEY")
+model_ls = openai.Model.list()
+
+model_names = [d["id"] for d in model_ls["data"] if "id" in d]
+gpt_options = filter(lambda string: "gpt" in string, model_names)
+model_str = list(gpt_options)
+model_display = dict(enumerate(model_str, 1))
+print(model_display)
+
+# avail_details = [d for d in model_ls["data"] if d["id"] in model_str]
+# print(avail_details)
+
+def model_prompt(models):
+    display = "\n".join([*['(' + str(k) + ') ' + str(v) for k,v in models.items()]])
+    while True:
+        try:
+            choice = int(input(f"Choose your model:\n{display}\n"))
+        except KeyboardInterrupt:
+            print("\nAuf Wiedersehen!")
+            sys.exit()
+        if choice in models:
+            return models[choice]
+        else:
+            print("Your choice is not available")
+
+
+def json_log(f_name, key, data):
+    with open(f_name, "r", encoding='utf-8') as jsonFile:
+        old_data = json.load(jsonFile)
+    old_data[key].append(data)
+    with open(f_name, "w") as jsonFile:
+        json.dump(old_data, jsonFile, ensure_ascii=False, indent=4)
 
 # Text to speech settings
 engine = pyttsx3.init()
 rate = engine.getProperty('rate')
 engine.setProperty('rate', rate)
+def talk(string):
+    engine.say(string)
+    engine.runAndWait()
 
 # Voice recognition init
 r = sr.Recognizer()
+r.operation_timeout = 5
+
+def transcribe():
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = r.listen(source, timeout=r.operation_timeout)
+    try:
+        print("thinking")
+        return r.recognize_whisper_api(audio, api_key=openai.api_key)
+    except sr.RequestError as e:
+        print("Could not request results from Whisper API")
+        return e
+    except sr.WaitTimeoutError as e:
+        print("Timeout error: " + str(e))
+        return e
 
 def speech_prompt():
     try:
@@ -32,27 +87,6 @@ def speech_prompt():
     except KeyboardInterrupt:
         print("\nAuf Wiedersehen!")
         sys.exit()
-
-def transcribe():
-    with sr.Microphone() as source:
-        print("Listening...")
-        audio = r.listen(source)
-    try:
-        print("thinking")
-        return r.recognize_whisper_api(audio, api_key=openai.api_key)
-    except sr.RequestError as e:
-        print("Could not request results from Whisper API")
-        return e
-
-# OpenAI init
-
-openai.api_key = os.getenv("OPENAI_API_KEY")
-model_ls = openai.Model.list()
-model_names = [d["id"] for d in model_ls if "id" in d]
-avail_models = filter(lambda string: "gpt" in string, model_names)
-model_str = ", ".join(list(avail_models))
-
-print(model_str)
 
 def startupCheck():
     if os.path.isfile(log_file) and os.access(log_file, os.R_OK):
@@ -63,58 +97,40 @@ def startupCheck():
         with io.open(os.path.join(".", log_file), 'w') as db_file:
             db_file.write(json.dumps({"chats": []}))
 
+
 def prompting(if_speech, context):
     if if_speech:
         try:
-            scribble = transcribe()
-            print("\033[3m{}\033[0m".format(scribble), "\n")
-            return scribble
+            preview = transcribe()
+            print("\033[3m{}\033[0m".format(preview), "\n")
+            return preview
         except KeyboardInterrupt:
-            session_log = {"start": start_time, "end": time_str(), "content": context}
-            json_log(log_file, "chats", session_log, "log")
+            log_content = {"start": start_time, "end": time_str(), "content": context}
+            json_log(log_file, "chats", log_content)
             print("\nAuf Wiedersehen!")
             sys.exit()
         except sr.RequestError:
             return input("We can't hear you at the moment, type here instead:\n")
     else:
-        return input("User (type \"new\" for session change):\n")
+        return input("User:\n")
 
 
-def json_log(f_name, key, data, mode):
-    match mode:
-        case "log":
-            with open(f_name, "r", encoding='utf-8') as jsonFile:
-                old_data = json.load(jsonFile)
-            old_data[key].append(data)
-            with open(f_name, "w") as jsonFile:
-                json.dump(old_data, jsonFile, ensure_ascii=False, indent=4)
-        case "overwrite":
-            with open(f_name, mode, encoding='utf-8') as f:
-                json.dump(data, f, ensure_ascii=False, indent=4)
-
-def persona_input(if_init, context):
-    display = ' | '.join(list(personas.keys()))
+def persona_input(options, if_continue, context):
+    display = "\n".join([*['(' + str(k) + ') ' + str(v) for k,v in options.items()]])
     while True:
         try:
-            choice = input(f"Choose your fighter:\n{display}\n")
+            choice = int(input(f"Choose your fighter:\n{display}\n"))
         except KeyboardInterrupt:
-            session_log = {"start": start_time, "end": time_str(), "content": context}
-            json_log(log_file, "chats", session_log, "log")
+            log_content = {"start": start_time, "end": time_str(), "content": context}
+            json_log(log_file, "chats", log_content)
             print("\nAuf Wiedersehen!")
             sys.exit()
-        if choice in personas:
-            if if_init:
-                try:
-                    context = [{"role": "system", "content": personas[choice]}]
-                except UnboundLocalError:
-                    print(str(context))
-                    sys.exit()
-            else:
-                session_log = {"start": start_time, "end": time_str(), "content": context}
-                json_log(log_file, "chats", session_log, "log")
-                context = [{"role": "system", "content": personas[choice]}]
+        if choice in options:
+            if if_continue:
+                log_content = {"start": start_time, "end": time_str(), "content": context}
+                json_log(log_file, "chats", log_content)
+            context = [{"role": "system", "content": personas[options[choice]]}]
             return choice, context
-            break
         else:
             print("Your choice is not available")
 
@@ -122,34 +138,34 @@ def persona_input(if_init, context):
 
 def main():
     startupCheck()
-
     context_arr = []
-
-    agent, context_arr = persona_input(True, context_arr)
     voice_opt = speech_prompt()
+    MODEL_ID = model_prompt(model_display)
+    agent, context_arr = persona_input(persona_display, False, context_arr)
+    print("(input \"new\" for session change)")
+
     while True:
         try:
             promptio = prompting(voice_opt, context_arr)
         except KeyboardInterrupt:
-            session_log = {"start": start_time, "end": time_str(), "content": context_arr}
-            json_log(log_file, "chats", session_log, "log")
+            log_content = {"start": start_time, "end": time_str(), "content": context_arr}
+            json_log(log_file, "chats", log_content)
             print("\nAuf Wiedersehen!")
             sys.exit()
-        if promptio == "new":
-            agent, context_arr = persona_input(False, context_arr)
+        if promptio.lower() == "new":
+            agent, context_arr = persona_input(persona_display, True, context_arr)
             continue
 
         context_arr.append({"role": "user", "content": promptio})
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
+            model=MODEL_ID,
             messages=context_arr
         )
 
         assist=response.choices[0].message
         cost=response.usage.total_tokens
-        print(f"\n{agent}:\n{assist.content}\nCost: USD {cost*0.002/1000}\n")
-        engine.say(assist.content)
-        engine.runAndWait()
+        print(f"\n{agent}:\n{assist.content}\nCost: {cost}\n")
+        talk(assist.content)
         context_arr.append(assist)
 
 main()
