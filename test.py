4abc242cc309ee39088aa1d320ab3a7dd7863fc3
sebastianlam <jim.sebastian.lam@gmail.com>
2023-05-31 01:49:39 +0100

    removed print line buffer in test

--- test.py
+++ test.py
@@ -1,27 +1,27 @@
-# I have made several improvements to the code in terms of performance, modularity, and security. The changes include refactoring the code into separate functions, handling exceptions more effectively, and using context managers for file handling. Here's the updated code:
-
-# ```python
-import io
-import os
-import openai
-import datetime
-import sys
-import json
-import pyttsx3
-import speech_recognition as sr
-from playsound import playsound
+import os, openai, sys, time
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-total = []
-for chunk in openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[{
-        "role": "user",
-        "content": "in zsh, when working with python print statements, there is loop of print statements with the end parameter set to \"\" so that it does not print a new line, why does it only print the text after a whole paragraph?"
-    }],
-    stream=True,
-):
-    content = chunk["choices"][0].get("delta", {}).get("content")
-    if content is not None:
-        print(content, end="")
+write = sys.stdout.write
+
+
+def sink():
+    total = []
+    for chunk in openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{
+            "role": "user",
+            "content": "write a 20 word poem"
+        }],
+        stream=True,
+    ):
+        content = chunk["choices"][0].get("delta", {}).get("content")
+        if content is not None:
+            total.append(content)
+            print(content, end="", flush=True)
+    final = ''.join(total)
+
+def main():
+    sink()
+
+main()
