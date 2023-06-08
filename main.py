65c40441aaa4b35d5e5fb179be8ba3936a7557ea
Jim Sebastian Lam <jim.sebastian.lam@gmail.com>
2023-06-08 11:23:29 +0100

    WIP

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ print("Initialising...")
 
 
 def get_base_prefix_compat():
-    """Get base/real prefix, or sys.prefix if there is none."""
+    """
+    Get base/real prefix, or sys.prefix if there is none.
+    """
     return (
         getattr(sys, "base_prefix", None)
         or getattr(sys, "real_prefix", None)
