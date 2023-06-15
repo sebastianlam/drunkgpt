bc9a1d7ecf88ff69b87ad7dfdf8cb71dbcf1fe41
Jim Sebastian Lam <jim.sebastian.lam@gmail.com>
2023-06-15 14:42:26 +0100

    support for more platforms

--- setup.sh
+++ setup.sh
@@ -9,14 +9,20 @@ else
     SHELL="sh"
 fi
 
+# Check for required commands
+# for cmd in sudo grep read; do
+#     if ! [ -x "$(command -v $cmd)" ]; then
+#         echo "Error: Required command '$cmd' not found." >&2
+#         exit 1
+#     fi
+# done
+
 # Check for OPENAI_API_KEY environment variable
 if [ -z "${OPENAI_API_KEY}" ]; then
     echo 'Warning: OPENAI_API_KEY environment variable not found.' >&2
     read -p 'Please enter your OpenAI API key: ' OPENAI_API_KEY
     export OPENAI_API_KEY
 fi
-
-echo $OPENAI_API_KEY
 
 # Check for poetry
 if ! [ -x "$(command -v poetry)" ]; then
@@ -25,40 +31,62 @@ if ! [ -x "$(command -v poetry)" ]; then
     exit 1
 fi
 
+# Set desired Python version (default to 3.11.3 if not specified)
+PYTHON_VERSION="${PYTHON_VERSION:-3.11.3}"
+
 # Check for pyenv
 if [ -x "$(command -v pyenv)" ]; then
-    # Check if Python 3.11.3 is installed
-    if ! pyenv versions | grep -q 3.11.3; then
-        pyenv install 3.11.3
+    # Check if desired Python version is installed
+    if ! pyenv versions | grep -q "$PYTHON_VERSION"; then
+        pyenv install "$PYTHON_VERSION"
+        if [ $? -ne 0 ]; then
+            echo "Error: Failed to install Python $PYTHON_VERSION using pyenv." >&2
+            exit 1
+        fi
     fi
-    # Set local Python version to 3.11.3
-    pyenv local 3.11.3
+
+    # Set local Python version to desired version
+    pyenv local "$PYTHON_VERSION"
 else
     echo 'Warning: pyenv is not installed. This may cause issues.' >&2
     echo 'Visit https://github.com/pyenv/pyenv#installation for instructions on how to install pyenv.' >&2
 fi
 
-python --version
-
 # Install portaudio package on Linux or macOS
 if [ -x "$(command -v apt-get)" ]; then
     # Debian/Ubuntu-based systems
-    sudo apt-get update
-    sudo apt-get install portaudio19-dev
-elif [ -x "$(command -v yum)" ]; then
-    # CentOS/RHEL-based systems
-    sudo yum install portaudio-devel
+    sudo apt-get update && sudo apt-get install portaudio19-dev || {
+        echo "Error: Failed to install portaudio using apt-get." >&2;
+        exit 1;
+    }
 elif [ -x "$(command -v dnf)" ]; then
-    # Fedora-based systems
-    sudo dnf install portaudio-devel
+    # Fedora-based systems or systems that support both yum and dnf (dnf preferred) 
+    sudo dnf install portaudio-devel || {
+        echo "Error: Failed to install portaudio using dnf." >&2;
+        exit 1;
+    }
+elif [ -x "$(command -v yum)" ]; then
+    # CentOS/RHEL-based systems 
+    sudo yum install portaudio-devel || {
+        echo "Error: Failed to install portaudio using yum." >&2;
+        exit 1;
+    }
+elif [ -x "$(command -v pacman)" ]; then
+    # Arch Linux-based systems 
+    sudo pacman -Syu portaudio || {
+        echo "Error: Failed to install portaudio using pacman." >&2;
+        exit 1;
+    }
 elif [ -x "$(command -v brew)" ]; then
     # Homebrew on Linux or macOS
-    brew install portaudio
+    brew install portaudio || {
+        echo "Error: Failed to install portaudio using brew." >&2;
+        exit 1;
+    }
 fi
 
 # Run pip commands within a poetry environment using Zsh or sh as appropriate.
 $SHELL <<EOF
-poetry install
-poetry run pip install wheel setuptools pip --upgrade 
-poetry run python -m pip install playsound 
+poetry run pip install wheel setuptools pip --upgrade
+poetry run python -m pip install playsound
 EOF
